6cc6d2c8890b7ceb171f304c862e095d513c49aa
Vaibhav <shanumac@Vaibhavs-MacBook-Pro.local>
2021-10-07 19:54:59 +0530

    first commit

---
+++ Screens/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text styles={{ fontSize: 30, color: "black" }}>Hi</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
